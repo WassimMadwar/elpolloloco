153d9b889d04b8b339c367472728bbd2ac4faa62
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-03 20:50:21 +0200

    animation for  Enemy start

--- script/model/enemy.class.js
+++ script/model/enemy.class.js
@@ -1,17 +1,31 @@
 class Enemy extends MovableObj {
   constructor(parameters) {
-    super().loadImg(
-      "assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
-    );
-    this.x =    150 + Math.random() * 100;
+    super();
+    this.loadImg("assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.x = 150 + Math.random() * 100;
     this.y = 100;
     this.height = 25;
     this.width = 15;
     this.animate();
   }
-    animate() {
+  animate() {
     setInterval(() => {
-        this.x -= 0.15;
-    }, 1000/60); // 60 FPS
+      this.x -= 0.15;
+    }, 1000 / 60); // 60 FPS
+  }
+
+  animateEnemy() {
+    setInterval(() => {
+      let path = this.imageCharWalking[this.currentImg];
+      this.img = this.imageCache[path];
+      this.updateCurrentImg();
+    }, 100);
+  }
+
+  updateCurrentImg() {
+    this.currentImg++;
+    if (this.currentImg >= this.imageCharWalking.length) {
+      this.currentImg = 0;
+    }
   }
 }
