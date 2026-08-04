09c313114566a18b98d9eb0edc3de91124a1c06c
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-04 19:00:09 +0200

    keyboard definition

--- script/script.js
+++ script/script.js
@@ -1,26 +1,41 @@
-console.log("Class Info");
-
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 let canvas;
-// let ctx;
-let game ;
+let game;
 let keyTaste = new KeyboardObj();
 
 function init() {
   canvas = document.getElementById("canvas");
-  game = new Game(canvas);
-  // character.src = "assets/img/2_character_pepe/1_idle/idle/I-1.png";
-  // ctx = canvas.getContext("2d");
-  // setTimeout(() => {
-  //   ctx.drawImage(character, 20, 20, 50, 100);
-  // }, 2000);
-  // console.log("gtgtt",game.character);
+  game = new Game(canvas, keyTaste);
+
   window.addEventListener("keydown", (e) => {
     if (e.key == "ArrowLeft") {
       keyTaste.left = true;
+    }
+    if (e.key == "ArrowRight") {
+      keyTaste.right = true;
+    }
+    if (e.key == "ArrowUp") {
+      keyTaste.up = true;
+    }
+    if (e.key == " ") {
+      keyTaste.space = true;
+    }
+  });
+  window.addEventListener("keyup", (e) => {
+    if (e.key == "ArrowLeft") {
+      keyTaste.left = false;
+    }
+    if (e.key == "ArrowRight") {
+      keyTaste.right = false;
+    }
+    if (e.key == "ArrowUp") {
+      keyTaste.up = false;
+    }
+    if (e.key == " ") {
+      keyTaste.space = false;
     }
   });
 }
