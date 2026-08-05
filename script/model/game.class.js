e6fbc06352fcab0b3f2b6703c314ae6af6ccc582
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-05 19:31:31 +0200

    add more long background

--- script/model/game.class.js
+++ script/model/game.class.js
@@ -11,13 +11,63 @@ class Game {
     new Cloud("assets/img/5_background/layers/4_clouds/2.png"),
   ];
   backgrounds = [
+    new BackgroundObj("assets/img/5_background/layers/air.png", -300),
+    new BackgroundObj(
+      "assets/img/5_background/layers/3_third_layer/2.png",
+      -300,
+    ),
+    new BackgroundObj(
+      "assets/img/5_background/layers/2_second_layer/2.png",
+      -300,
+    ),
+    new BackgroundObj(
+      "assets/img/5_background/layers/1_first_layer/2.png",
+      -300,
+    ),
     new BackgroundObj("assets/img/5_background/layers/air.png", 0),
     new BackgroundObj("assets/img/5_background/layers/3_third_layer/1.png", 0),
     new BackgroundObj("assets/img/5_background/layers/2_second_layer/1.png", 0),
     new BackgroundObj("assets/img/5_background/layers/1_first_layer/1.png", 0),
+    new BackgroundObj("assets/img/5_background/layers/air.png", 300),
+    new BackgroundObj(
+      "assets/img/5_background/layers/3_third_layer/2.png",
+      300,
+    ),
+    new BackgroundObj(
+      "assets/img/5_background/layers/2_second_layer/2.png",
+      300,
+    ),
+    new BackgroundObj(
+      "assets/img/5_background/layers/1_first_layer/2.png",
+      300,
+    ),
+    new BackgroundObj("assets/img/5_background/layers/air.png", 300 * 2),
+    new BackgroundObj(
+      "assets/img/5_background/layers/3_third_layer/2.png",
+      300 * 2,
+    ),
+    new BackgroundObj(
+      "assets/img/5_background/layers/2_second_layer/2.png",
+      300 * 2,
+    ),
+    new BackgroundObj(
+      "assets/img/5_background/layers/1_first_layer/2.png",
+      300 * 2,
+    ),
+    new BackgroundObj("assets/img/5_background/layers/air.png", 300 * 3),
+    new BackgroundObj(
+      "assets/img/5_background/layers/3_third_layer/2.png",
+      300 * 3,
+    ),
+    new BackgroundObj(
+      "assets/img/5_background/layers/2_second_layer/2.png",
+      300 * 3,
+    ),
+    new BackgroundObj(
+      "assets/img/5_background/layers/1_first_layer/2.png",
+      300 * 3,
+    ),
   ];
-
-
 
   constructor(canvas, keyTaste) {
     this.renderCanvas = canvas;
@@ -25,7 +75,7 @@ class Game {
     this.keyAction = keyTaste;
     this.draw();
     this.setupGame();
-    this.camera_x ;
+    this.camera_x;
   }
 
   setupGame() {
@@ -34,12 +84,12 @@ class Game {
 
   draw() {
     this.ctx.clearRect(0, 0, this.renderCanvas.width, this.renderCanvas.height);
-    this.ctx.translate(this.camera_x,0);
+    this.ctx.translate(this.camera_x, 0);
     this.addObjectsToMap(this.backgrounds);
     this.addToMap(this.character);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemies);
-    this.ctx.translate(-this.camera_x,-0);
+    this.ctx.translate(-this.camera_x, -0);
 
     let self = this;
     requestAnimationFrame(() => self.draw());
@@ -68,11 +118,13 @@ class Game {
       this.flipImageBack(movableObj);
     }
   }
+
   flipImage(movableObj) {
     this.ctx.save();
     this.ctx.translate(movableObj.x * 2 + movableObj.width, 0);
     this.ctx.scale(-1, 1);
   }
+  
   flipImageBack(movableObj) {
     this.ctx.restore();
   }
