c53031933cd6f2208838235c493ef9e4fe7435bb
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-05 18:40:13 +0200

    mirroring the Character image

--- script/model/game.class.js
+++ script/model/game.class.js
@@ -48,14 +48,10 @@ class Game {
   addToMap(movableObj) {
     if (movableObj.otherDirection) {
       this.ctx.save();
-      this.ctx.translate(movableObj.img.width, 0);
+      this.ctx.translate(movableObj.x * 2 + movableObj.width, 0);
       this.ctx.scale(-1, 1);
-      // movableObj.x = movableObj.x * -1;
     }
-    if (movableObj.otherDirection) {
-      // movableObj.x = movableObj.x * -1;
-      this.ctx.restore();
-    }
+
     this.ctx.drawImage(
       movableObj.img,
       movableObj.x,
@@ -64,5 +60,8 @@ class Game {
       movableObj.height,
     );
 
+    if (movableObj.otherDirection) {
+      this.ctx.restore();
+    }
   }
 }
