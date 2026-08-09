a793e6b7dd48677d0d3e2f2993beff792febe67f
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-09 19:11:48 +0200

    Implement ThrowableObj class with initial properties and update canvas height in CSS

--- script/model/throwableObj.class.js
+++ script/model/throwableObj.class.js
@@ -1,7 +1,11 @@
-class ThrowableObj extends MovableObj{
-    constructor(parameters) {
-        
-    }
-    throwBatell(){};
-    
+class ThrowableObj extends MovableObj {
+  constructor() {
+    super();
+    this.x = 100;
+    this.y = 100;
+    this.loadImg("assets/img/6_salsa_bottle/salsa_bottle.png");
+    this.height = 25;
+    this.width = 15;
+  }
+  throwBatell() {}
 }
