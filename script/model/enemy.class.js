6c2a68067d12c7673adfd7a55d4c69f883a1d788
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-11 22:29:38 +0200

    add fix distance to the enemy

--- script/model/enemy.class.js
+++ script/model/enemy.class.js
@@ -5,15 +5,16 @@ class Enemy extends MovableObj {
     "assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
   ];
   currentImg = 0;
-  constructor(parameters) {
+  constructor(x) {
     super();
     this.loadImg("assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
-    this.x = 150 + Math.random() * 100;
+    this.x = x;
     this.y = 100;
     this.height = 25;
     this.width = 15;
     this.loadImgMoving(this.imgEnemyWalking);
-    this.speed = 0.15 + Math.random() * 1.1;
+    // this.speed = 0.15 + Math.random() * 1.1;
+    // this.speed = 0.15 + Math.random() * 1.1;
     this.animateEnemyWalking();
     this.animate();
   }
