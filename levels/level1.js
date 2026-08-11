c2417e49aa1578ce8e77072e947eaac582b4431f
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-11 22:45:58 +0200

    enhancing chicken  Distance add dynamically

--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,5 @@
 const level1 = new Level(
-  [new Enemy(300), new Enemy(350), new Enemy(400), new Endboss()],
+  [new Enemy(), new Enemy(), new Enemy(), new Endboss()],
   [
     new Cloud("assets/img/5_background/layers/4_clouds/1.png"),
     new Cloud("assets/img/5_background/layers/4_clouds/2.png"),
--- script/model/enemy.class.js
+++ script/model/enemy.class.js
@@ -4,17 +4,17 @@ class Enemy extends MovableObj {
     "assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
     "assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
   ];
-  currentImg = 0;
-  constructor(x) {
+  // currentImg = 0;
+  static countchicken = 0;
+  constructor() {
     super();
     this.loadImg("assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
-    this.x = x;
+    this.x = 300 + Enemy.countchicken * 100;
+    Enemy.countchicken++;
     this.y = 100;
     this.height = 25;
     this.width = 15;
     this.loadImgMoving(this.imgEnemyWalking);
-    // this.speed = 0.15 + Math.random() * 1.1;
-    // this.speed = 0.15 + Math.random() * 1.1;
     this.animateEnemyWalking();
     this.animate();
   }
