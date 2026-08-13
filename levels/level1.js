5214661eeebea585abb84c0ea7b4bfc1836b6022
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-13 21:55:39 +0200

    add more enemies More clouds with Distance

--- levels/level1.js
+++ levels/level1.js
@@ -1,10 +1,15 @@
 const level1 = new Level(
-  [new Enemy(), new Enemy(), new Enemy(), new Endboss()],
+  [new Enemy(), new Enemy(), new Enemy(),new Enemy(),new Enemy(),new Enemy(), new Endboss()],
   [
-    new Cloud("assets/img/5_background/layers/4_clouds/1.png"),
-    new Cloud("assets/img/5_background/layers/4_clouds/2.png"),
-    new Cloud("assets/img/5_background/layers/4_clouds/1.png"),
-    new Cloud("assets/img/5_background/layers/4_clouds/2.png"),
+    new Cloud("assets/img/5_background/layers/4_clouds/1.png", 0),
+    new Cloud("assets/img/5_background/layers/4_clouds/2.png", 225),
+    new Cloud("assets/img/5_background/layers/4_clouds/1.png", 450),
+    new Cloud("assets/img/5_background/layers/4_clouds/2.png", 775),
+    new Cloud("assets/img/5_background/layers/4_clouds/1.png", 900),
+    new Cloud("assets/img/5_background/layers/4_clouds/2.png", 1225),
+    new Cloud("assets/img/5_background/layers/4_clouds/1.png", 1550),
+    new Cloud("assets/img/5_background/layers/4_clouds/2.png", 1875),
+    new Cloud("assets/img/5_background/layers/4_clouds/1.png", 2150),
   ],
   [
     new BackgroundObj("assets/img/5_background/layers/air.png", -300),
