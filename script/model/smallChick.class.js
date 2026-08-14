89b2404d695b43fd9352f2dddfd1ac8156389ce1
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-14 20:21:42 +0200

    create small chick class

---
+++ script/model/smallChick.class.js
@@ -0,0 +1,32 @@
+class SmallChick extends Enemy {
+  imgEnemyWalking = [
+    "assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+    "assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+    "assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+  ];
+
+  constructor(x, y) {
+    super();
+    this.setupSmallChick(x, y);
+  }
+
+  setupSmallChick(x, y) {
+    clearInterval(this.walkInterval);
+    this.x = x;
+    this.y = y;
+    this.width = 10;
+    this.height = 15;
+    this.loadImg(this.imgEnemyWalking[0]);
+    this.loadImgMoving(this.imgEnemyWalking);
+    this.animateEnemyWalking();
+  }
+
+  die() {
+    if (this.isDying) return;
+    this.isDying = true;
+    this.deathTime = new Date().getTime();
+    clearInterval(this.walkInterval);
+    clearInterval(this.moveInterval);
+    this.loadImg("assets/img/3_enemies_chicken/chicken_small/2_dead/dead.png");
+  }
+}
