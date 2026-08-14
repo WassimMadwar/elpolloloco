75981401a7970c3e1be6a50323302caaf11f1452
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-14 20:06:55 +0200

    add cloud befor End boss

--- levels/level1.js
+++ levels/level1.js
@@ -10,6 +10,7 @@ const level1 = new Level(
     new Cloud("assets/img/5_background/layers/4_clouds/1.png", 1550),
     new Cloud("assets/img/5_background/layers/4_clouds/2.png", 1875),
     new Cloud("assets/img/5_background/layers/4_clouds/1.png", 2150),
+      new Cloud("assets/img/5_background/layers/4_clouds/2.png", 2675),
   ],
   [
     new BackgroundObj("assets/img/5_background/layers/air.png", -300),
