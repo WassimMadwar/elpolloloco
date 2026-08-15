85a9d84da1f9bf10bdb96e591871b58ff434f0b5
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-15 08:50:24 +0200

    start hurt  End boss

--- script/model/endboss.class.js
+++ script/model/endboss.class.js
@@ -13,11 +13,25 @@ class Endboss extends MovableObj {
     "assets/img/4_enemie_boss_chicken/2_alert/G11.png",
     "assets/img/4_enemie_boss_chicken/2_alert/G12.png",
   ];
-  
+  imgBossHurt = [
+    "assets/img/4_enemie_boss_chicken/4_hurt/G21.png",
+    "assets/img/4_enemie_boss_chicken/4_hurt/G22.png",
+    "assets/img/4_enemie_boss_chicken/4_hurt/G23.png",
+  ];
+  imgBossDead = [
+    "assets/img/4_enemie_boss_chicken/5_dead/G24.png",
+    "assets/img/4_enemie_boss_chicken/5_dead/G25.png",
+    "assets/img/4_enemie_boss_chicken/5_dead/G26.png",
+  ];
+  hitsTaken = 0;
+  maxHits = 10;
+
   constructor() {
     super();
     this.loadImg("assets/img/4_enemie_boss_chicken/2_alert/G5.png");
     this.loadImgMoving(this.imgBossWalking);
+    this.loadImgMoving(this.imgBossHurt);
+    this.loadImgMoving(this.imgBossDead);
     this.animate();
     this.startThrowingChicks();
   }
@@ -26,6 +40,16 @@ class Endboss extends MovableObj {
     setInterval(() => {
       this.playAnimation(this.imgBossWalking);
     }, 200);
+  }
+
+  hit() {
+    if (this.isDying) return;
+    this.hitsTaken++;
+    if (this.hitsTaken >= this.maxHits) {
+      this.die();
+    } else {
+      this.lastHit = new Date().getTime();
+    }
   }
 
   startThrowingChicks() {
