e9e7a3a77e0144f129d02ad1c342e9dc692d5690
Wassim Madwar <wassimmadwar@gmail.com>
2026-08-23 18:21:12 +0200

    nix

--- script/model/controls.class.js
+++ script/model/controls.class.js
@@ -100,7 +100,7 @@ class Control {
   }
 
   drawStartPanel() {
-    this.ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
+    this.ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
     this.ctx.fillRect(
       this.getStartPanelX(),
       this.getStartPanelY(),
